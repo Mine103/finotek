d627f15e77987455b0c61077fa555f48490fcf62
seobcom0 <seobcom0@gmail.com>
2022-07-04 18:05:59 +0900

    2022-07-04

---
+++ spring5/sp5-chap08/src/main/java/spring/MemberDao.java
@@ -0,0 +1,23 @@
+/*
+ * DB연동하는법은 배우지 않았으므로 자바의 Map을 이용하여 구현.
+ * DB연동은 8장에서 학습*/
+package spring;
+
+import java.util.Collection;
+
+public class MemberDao {
+	
+	public Member selectByEmail(String email) {
+		return null;
+	}
+	
+	public void insert(Member member) {
+	}
+	
+	public void update(Member member) {
+	}
+	
+	public Collection<Member> selectAll() {
+		return null;
+	}
+}
